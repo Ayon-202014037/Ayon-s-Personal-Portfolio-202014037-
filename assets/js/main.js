9b6ecffe44f0995bbfce31efe7e88f69b88de906
Ayon-202014037 <63556252+Ayon-202014037@users.noreply.github.com>
2021-12-27 11:15:22 +0600

    Firebase Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -245,3 +245,26 @@
   });
 
 })()
+
+
+function show() {
+
+  var name = document.getElementById("name").value;
+  var email= document.getElementById("email").value;
+  var subject= document.getElementById("subject").value;
+  var message= document.getElementById("message").value;
+
+
+   firebase.database().ref('User/' + name).set({
+          name : name,
+          email : email,
+          subject : subject,
+          message : message
+        }, function(error) {
+          if (error) {
+            // The write failed...
+          } else {
+              alert("DONE");
+          }
+        });
+} 
