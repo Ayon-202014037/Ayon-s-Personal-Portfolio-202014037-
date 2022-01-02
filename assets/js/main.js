26f4af727cd7d8a85cbe13c64bd9210199823011
Ayon-202014037 <63556252+Ayon-202014037@users.noreply.github.com>
2022-01-02 21:01:37 +0600

    Contact Section Modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -317,31 +317,5 @@ function image(img)
   window.open(src);
 }
 
-function show()
-{
-  var name=document.getElementById("name").value;
-  var email=document.getElementById("email").value;
-  var message=document.getElementById("message").value;
-  var subject=document.getElementById("subject").value;
-
-  firebase.database().ref('User/'+name).set({
-    name:name,
-    email:email,
-    message:message,
-    subject:subject
-  },function(error){
-    if(error){
-
-    } else{
-      alert("DONE");
-    }
-  }
-
-  );
-
-
-  
-}
-
 
 
