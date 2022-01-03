dfdf2829fc7dd6b9a89a6b09bcc3608190cda002
Ayon-202014037 <63556252+Ayon-202014037@users.noreply.github.com>
2022-01-03 10:04:33 +0600

    Blogs Section to be continued-3

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -15,6 +15,38 @@
 
      } else{
        alert("Your response has been recorded. Thanks for your valuable time!");
+       document.getElementById("name").value="";
+       document.getElementById("email").value="";
+       document.getElementById("message").value="";
+       document.getElementById("subject").value="";
+     }
+   }
+
+   );
+ }
+
+ function Comment()
+ {
+   var name=document.getElementById("name").value;
+   var email=document.getElementById("email").value;
+   var message=document.getElementById("message").value;
+   var subject=document.getElementById("subject").value;
+
+   firebase.database().ref('Comment_Blog/'+name).set({
+     name:name,
+     email:email,
+     message:message,
+     subject:subject
+   },function(error){
+     if(error){
+
+     } else{
+       alert("Your response has been recorded. Thanks for your valuable time!");
+       document.getElementById("name").value="";
+       document.getElementById("email").value="";
+       document.getElementById("message").value="";
+       document.getElementById("subject").value="";
+       
      }
    }
 
