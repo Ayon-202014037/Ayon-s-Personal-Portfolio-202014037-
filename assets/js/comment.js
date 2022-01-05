a124c527b28a1d2621b5b0134a9b99d03df61a30
Ayon-202014037 <63556252+Ayon-202014037@users.noreply.github.com>
2022-01-05 07:42:39 +0600

    Comment,Project Section modified

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -1,29 +1,29 @@
- function show()
- {
-   var name=document.getElementById("name").value;
-   var email=document.getElementById("email").value;
-   var message=document.getElementById("message").value;
-   var subject=document.getElementById("subject").value;
+function show()
+{
+  var name=document.getElementById("name").value;
+  var email=document.getElementById("email").value;
+  var message=document.getElementById("message").value;
+  var subject=document.getElementById("subject").value;
 
-   firebase.database().ref('User/'+name).set({
-     name:name,
-     email:email,
-     message:message,
-     subject:subject
-   },function(error){
-     if(error){
+  firebase.database().ref('User/'+name).set({
+    name:name,
+    email:email,
+    message:message,
+    subject:subject
+  },function(error){
+    if(error){
 
-     } else{
-       alert("Your response has been recorded. Thanks for your valuable time!");
-       document.getElementById("name").value="";
-       document.getElementById("email").value="";
-       document.getElementById("message").value="";
-       document.getElementById("subject").value="";
-     }
-   }
+    } else{
+      alert("Your response has been recorded. Thanks for your valuable time!");
+     
+    }
+  }
 
-   );
- }
+  );
+}
+
+
+
 
  function Comment()
  {
@@ -41,7 +41,7 @@
      if(error){
 
      } else{
-       alert("Your response has been recorded. Thanks for your valuable time!");
+       alert("আপনার মূলবান মতামত দেবার জন্য ধন্যবাদ!");
        document.getElementById("name").value="";
        document.getElementById("email").value="";
        document.getElementById("message").value="";
--- assets/js/main.js
+++ assets/js/main.js
@@ -319,3 +319,26 @@ function image(img)
 
 
 
+function show()
+{
+  var name=document.getElementById("name").value;
+  var email=document.getElementById("email").value;
+  var message=document.getElementById("message").value;
+  var subject=document.getElementById("subject").value;
+
+  firebase.database().ref('User/'+name).set({
+    name:name,
+    email:email,
+    message:message,
+    subject:subject
+  },function(error){
+    if(error){
+
+    } else{
+      alert("Your response has been recorded. Thanks for your valuable time!");
+     
+    }
+  }
+
+  );
+}
