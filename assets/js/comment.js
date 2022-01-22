6b87eec8115811aee14691c374de26647034012b
Ayon-202014037 <63556252+Ayon-202014037@users.noreply.github.com>
2022-01-22 15:11:48 +0600

    Update comment.js

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -10,12 +10,10 @@ const firebaseConfig =
   measurementId: "G-51J2VRR8HK"
 };
 
-  // Initialize Firebase
-  //const app = initializeApp(firebaseConfig);
+ 
   firebase.initializeApp(firebaseConfig);
 
 let contactInfo = firebase.database().ref("Comment_Blog");
-//let contactInfo2 = firebase.database().ref("Comment_Blog_2");
 
 
 document.querySelector(".blog-form").addEventListener("submit",Comment);
@@ -24,14 +22,12 @@ document.querySelector(".blog-form").addEventListener("submit",Comment);
 function Comment(e)
 {
   e.preventDefault();
-  //console.log();
 
   let name = document.querySelector("#name").value;
   let email = document.querySelector("#email").value;
   let subject = document.querySelector("#subject").value;
   let message = document.querySelector("#message").value;
 
-  // console.log(name,email,subject,message);
 
   saveContactInfo(name,email,subject,message);
 
@@ -189,123 +185,3 @@ retrieveInfos_2();
 
 
 
-
-/*let contactInfo2 = firebase.database().ref("User");
-  
-  document.querySelector(".contact-comment").addEventListener("submit",show);
-  
-  
-  function show(e2)
-  {
-    e2.preventDefault();
-    //console.log();
-  
-    let name2 = document.querySelector("#name").value;
-    let email2 = document.querySelector("#email").value;
-    let subject2 = document.querySelector("#subject").value;
-    let message2 = document.querySelector("#message").value;
-  
-    // console.log(name,email,subject,message);
-  
-    saveContactInfo2(name2,email2,subject2,message2);
-  
-    document.querySelector(".contact-comment").reset();
-  }
-  
-  function saveContactInfo2(name2,email2,subject2,message2)
-  {
-    let newContactInfo2 = contactInfo2.push();
-  
-  
-  
-    newContactInfo2.set({
-      name: name2,
-      email: email2,
-      subject: subject2,
-      message: message2
-    });
-    retrieveInfos2();
-  }
-  
-  
-  function retrieveInfos2()
-  {
-    let ref2 = firebase.database().ref("User");
-    ref2.on("value",gotData2);
-  }
-  
-  
-  
-  function gotData2(data2)
-  {
-    let info2 = data2.val();
-    let keys2 = Object.keys(info2);
-  
-    for(let i = 0; i < keys2.length; i++)
-    {
-        let infoData2 = keys2[i];
-        let name2 = info2[infoData2].name;
-        let email2 = info2[infoData2].email;
-        let subject2 = info2[infoData2].subject;
-        let message2 = info2[infoData2].message;
-  
-        console.log(name2,email2,subject2,message2);
-        
-        let infosResults2 = document.querySelector(".infosResults2");
-  
-      infosResults2.innerHTML += `<div>
-      <p><strong>Name: <strong/>${name2} <br/>
-      <a><strong>Email: <strong/>${email2}</a> <br/>
-      <a><strong>Subject: <strong/>${subject2}</a> <br/>
-      <a><strong>Message: <strong/>${message2}</a> 
-      </p>
-      </div>`;
-    }
-  }
-  
-  //retrieveInfos2();
-  
-  
-  
-  
-  
-  
-   
-  
-  
-  
-  
-  
-  
-  
-  
-   
-
-
-
-
-
-
- /*function show()
-{
-  var name=document.getElementById("name").value;
-  var email=document.getElementById("email").value;
-  var message=document.getElementById("message").value;
-  var subject=document.getElementById("subject").value;
-
-  firebase.database().ref('User/'+name).set({
-    name:name,
-    email:email,
-    message:message,
-    subject:subject
-  },function(error){
-    if(error){
-
-    } else{
-      alert("Your response has been recorded. Thanks for your valuable time!");
-     
-    }
-  }
-
-  );
-}*/
